4cc2e6eac5e9cb2b170fba6f6f673fa7d0306982
HassanAMian <hassanmian@hassanmian.net>
2015-01-10 02:27:50 -0500

    can now edit and destroy pins

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,34 +1,50 @@
 class PinsController < ApplicationController
 before_action :find_pin, only: [:show, :edit, :update, :destroy]
 
-	def index
-		@pins = Pin.all.order("created_at DESC")
+def index
+	@pins = Pin.all.order("created_at DESC")
+end
+
+def show
+end
+
+def new
+	@pin = current_user.pins.build
+end
+
+def create
+	@pin = current_user.pins.build(pin_params)
+
+	if @pin.save
+		redirect_to @pin, notice: "Pin successfully posted!"
+	else
+		render 'new'
 	end
+end
 
-	def show
+def edit
+end
+
+def update
+	if @pin.update(pin_params)
+		redirect_to @pin, notice: "Pin successfully updated"
+	else
+		render 'edit'
 	end
+end
 
-	def new
-		@pin = Pin.new
-	end
+def destroy
+	@pin.destroy
+		redirect_to root_path
+end
+end
 
-	def create
-		@pin = Pin.new(pin_params)
+private
 
-		if @pin.save
-			redirect_to @pin, notice: "Successfully created new Pin!"
-		else
-			render 'new'
-		end
-	end
+def pin_params
+	params.require(:pin).permit(:title, :description, :image)
+end
 
-	private
-
-	def pin_params
-		params.require(:pin).permit(:title, :description)
-	end
-
-	def find_pin
-		@pin = Pin.find(params[:id])
-	end
+def find_pin
+	@pin = Pin.find(params[:id])
 end
